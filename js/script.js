46afff5f8f9ef605c10be722acd1e219a1771c39
wawan-setiawan-frig <fr.wawansetiawan@gmail.com>
2021-10-25 15:49:35 +0800

    second push

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@
 // Cool loopy loop of adjectives I like :)
 $(document).ready(function() {
 
-  var items = ["product leader", "women's advocate", "SQL-monkey", "python scripter", "data leader", "startup builder"],
+  var items = ["Programming", "Community development", "Sustainable Energy", "Reading Book", "Adventuring"],
       $text = $( '#about-me span' ),
       delay = 2; //seconds
   
